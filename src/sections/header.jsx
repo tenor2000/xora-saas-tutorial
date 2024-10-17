43b896f0ad66310d7134e2105f9aa9e604de4c85
tenor2000 <tenor2000@gmail.com>
2024-10-17 14:50:35 -0400

    made the header a bit more dynamic

--- src/sections/header.jsx
+++ src/sections/header.jsx
@@ -1,19 +1,43 @@
 import clsx from "clsx";
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Link as LinkScroll } from "react-scroll"
 
-const NavLink = ({ title }) => (
-  <LinkScroll
-    className="base-bold text-p4 upercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5">
-    {title}
-  </LinkScroll>
-)
-
 const Header = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [ hasScrolled, setHasScrolled ] = useState(false);
+  const [ isOpen, setIsOpen ] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setHasScrolled(window.scrollY > 32);
+    }
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);}
+  }, []);
+
+  const NavLink = ({ title }) => (
+    <LinkScroll
+      onClick={() => setIsOpen(false)}
+      to={title}
+      offset={-100}
+      spy
+      smooth
+      activeClass="nav-active"
+      className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5"
+    >
+      {title}
+    </LinkScroll>
+  )
 
   return (
-    <header className="fixed top-0 left-0 z-50 w-full py-10">
+    <header 
+      className={clsx(
+        "fixed top-0 left-0 z-50 w-full py-10 transition-all duration-500 max-lg:py-4", 
+        hasScrolled && 'py-2 bg-black-100 backdrop-blue-[8px]',
+      )}
+    >
       <div className="container flex h-14 items-center max-lg:px-5">
         <a className="lg:hidden flex-1 cursor-pointer z-2">
           <img
@@ -24,25 +48,30 @@ const Header = () => {
           />
         </a>
 
-        <div className={clsx('w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0', isOpen ? 'max-lg:opacity-100' : "max-lg:pointer-events-none")}>
-          <div className=
-            "max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4"
-          >
+        <div 
+          className={clsx(
+            'w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0', 
+            isOpen ? 'max-lg:opacity-100' : "max-lg:pointer-events-none"
+          )}
+        >
+          <div className="max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4">
             <nav className="max-lg:relative max-lg:z-2 max-lg:my-auto">
               <ul className="flex max-lg:block max-lg:px-12">
                 <li className="nav-li">
-                  <NavLink title="Features"/>
-                    <div className='dot' />
-                  <NavLink title="Pricing"/>
+                  <NavLink title="features"/>
+                  <div className='dot' />
+                  <NavLink title="pricing"/>
                 </li>
 
                 <li className="nav-logo">
                   <LinkScroll
                     to="hero"
-                    offset={-100}
+                    offset={-250}
                     spy
                     smooth
-                    className={clsx("max-lg:hidden transition-transform duration-500")}
+                    className={clsx(
+                      "max-lg:hidden transition-transform duration-500 cursor-pointer"
+                    )}
                   >
                     <img
                       src="/images/xora.svg"
@@ -54,14 +83,14 @@ const Header = () => {
                 </li>
 
                 <li className='nav-li'>
-                  <NavLink title="FAQ"/>
+                  <NavLink title="faq"/>
                   <div className='dot' />
-                  <NavLink title="Download"/>
+                  <NavLink title="download"/>
                 </li>
               </ul>
             </nav>
 
-            <div className="lg-hidden block absolute top-1/2 left-0 w-[960px] h-[380px] translate-x-[-290px] -translate-y-1/2 rotate-90">
+            <div className="lg:hidden block absolute top-1/2 left-0 w-[960px] h-[380px] translate-x-[-290px] -translate-y-1/2 rotate-90">
               <img 
                 src="/images/bg-outlines.svg" 
                 width={960} 
